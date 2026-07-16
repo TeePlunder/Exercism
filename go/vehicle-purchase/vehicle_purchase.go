c69d9223027063d0e50b34c8cbb752e873593b5f
TeePlunder <leon.bergmann23@gmail.com>
2026-07-16 09:42:52 +0200

    feat(go): solution for vehicle purchase

--- go/vehicle-purchase/vehicle_purchase.go
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -1,25 +1,35 @@
 package purchase
 
+import "fmt"
+
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	if kind == "car" || kind == "truck"{ return true}
-    return false
+	if kind == "car" || kind == "truck" {
+		return true
+	}
+	return false
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	if option1 == "Wuling Hongguang" && option2 == "Toyota Corolla" {
-        return "Toyota Corolla is clearly the better choice."
-    }
+	chosen := min(option1, option2)
 
-    if option1 == "Volkswagen Beetle" && option2 == "Volkswagen Golf" {
-        return "Volkswagen Beetle is clearly the better choice."
-    }
-
-    return ""
+	return fmt.Sprintf("%s is clearly the better choice.", chosen)
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+	if age < 3 {
+		return originalPrice * 0.8
+	}
+
+	if age >= 3 && age < 10 {
+		return originalPrice * 0.7
+	}
+
+	if age >= 10 {
+		return originalPrice * 0.5
+	}
+
+	return originalPrice
 }
