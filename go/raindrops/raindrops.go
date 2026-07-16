5c4a4d11e3f35cca0fa05afbb2d13b0da082dfeb
TeePlunder <leon.bergmann23@gmail.com>
2026-07-16 09:42:53 +0200

    feat(go): solution for raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,27 @@
 package raindrops
 
+import "strconv"
+
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	addPling := number%3 == 0
+	addPlang := number%5 == 0
+	addPlong := number%7 == 0
+
+	if !addPling && !addPlang && !addPlong {
+		return strconv.Itoa(number)
+	}
+
+	var result string
+
+	if addPling {
+		result += "Pling"
+	}
+	if addPlang {
+		result += "Plang"
+	}
+	if addPlong {
+		result += "Plong"
+	}
+
+	return result
 }
