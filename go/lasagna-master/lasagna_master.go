46dcef67d2ab2898ed6865be57b3c3932ea14efa
TeePlunder <leon.bergmann23@gmail.com>
2026-07-17 14:59:43 +0200

    feat(go): solution lasagna master

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,12 +1,50 @@
 package lasagnamaster
 
-// TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, preperationTimePerLayer int) int {
+	if preperationTimePerLayer == 0 {
+		preperationTimePerLayer = 2
+	}
 
-// TODO: define the 'Quantities()' function
+	return len(layers) * preperationTimePerLayer
+}
 
-// TODO: define the 'AddSecretIngredient()' function
+func Quantities(layers []string) (noodles int, souce float64) {
+	for _, layer := range layers {
+		if layer == "noodles" {
+			noodles += 50
+			continue
+		}
 
-// TODO: define the 'ScaleRecipe()' function
+		if layer == "sauce" {
+			souce += 0.2
+		}
+	}
+
+	return
+}
+
+func AddSecretIngredient(friendsList, myList []string) {
+	lastIndexOfMyList := len(myList) - 1
+	lastIndexOfFriendsList := len(friendsList) - 1
+
+	myList[lastIndexOfMyList] = friendsList[lastIndexOfFriendsList]
+}
+
+// quantities := []float64{ 1.2, 3.6, 10.5 }
+// scaledQuantities := ScaleRecipe(quantities, 4)
+// // => []float64{ 2.4, 7.2, 21 }
+
+func ScaleRecipe(quantities []float64, portions int) []float64 {
+	newQuantities := []float64{}
+
+	for _, quantity := range quantities {
+		onePortionQuantity := quantity / 2
+		newValue := onePortionQuantity * float64(portions)
+		newQuantities = append(newQuantities, newValue)
+	}
+
+	return newQuantities
+}
 
 // Your first steps could be to read through the tasks, and create
 // these functions with their correct parameter lists and return types.
