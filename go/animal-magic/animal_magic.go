a0b66bfc17188b39b2c012e9618f1f9b5a8377b9
TeePlunder <leon.bergmann23@gmail.com>
2026-07-17 08:57:42 +0200

    feat(go): solution animal magic

--- go/animal-magic/animal_magic.go
+++ go/animal-magic/animal_magic.go
@@ -1,16 +1,29 @@
 package chance
 
+import "math/rand"
+
 // RollADie returns a random int d with 1 <= d <= 20.
 func RollADie() int {
-	panic("Please implement the RollADie function")
+	value := rand.Intn(20)
+	if value == 0 {
+		return 1
+	}
+
+	return value
 }
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	return rand.Float64() * 12
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order.
 func ShuffleAnimals() []string {
-	panic("Please implement the ShuffleAnimals function")
+	animals := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+
+	rand.Shuffle(len(animals), func(i, j int) {
+		animals[i], animals[j] = animals[j], animals[i]
+	})
+
+	return animals
 }
