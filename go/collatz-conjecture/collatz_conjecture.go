ce9cd3d7974343d9102b8b06e898db411268dbd0
TeePlunder <leon.bergmann23@gmail.com>
2026-07-17 08:02:24 +0200

    feat(go): solution collatz-conjecture

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,25 @@
 package collatzconjecture
 
+import (
+	"errors"
+)
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	currentNumber := n
+	steps := 0
+
+	if currentNumber <= 0 {
+		return 0, errors.New("input must be a positive integer")
+	}
+
+	for currentNumber != 1 {
+		if currentNumber%2 == 0 {
+			currentNumber /= 2
+		} else {
+			currentNumber = currentNumber*3 + 1
+		}
+		steps++
+	}
+
+	return steps, nil
 }
