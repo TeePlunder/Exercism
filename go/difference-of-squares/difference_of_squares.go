8b404d2d26d4de4a1f75fd8e5964b65a97264a2b
TeePlunder <leon.bergmann23@gmail.com>
2026-07-21 09:15:55 +0200

    feat(go): submit differnece of squares

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,13 +1,32 @@
 package differenceofsquares
 
+import (
+	"math"
+)
+
 func SquareOfSum(n int) int {
-	panic("Please implement the SquareOfSum function")
+	sum := 0
+	for i := 0; i <= n; i++ {
+		sum += i
+	}
+
+	return int(math.Pow(float64(sum), 2))
 }
 
 func SumOfSquares(n int) int {
-	panic("Please implement the SumOfSquares function")
+	sum := 0
+	for i := 0; i <= n; i++ {
+		powResult := int(math.Pow(float64(i), 2))
+		sum += powResult
+	}
+
+	return sum
 }
 
 func Difference(n int) int {
-	panic("Please implement the Difference function")
+	squareOfSum := SquareOfSum(n)
+
+	sumOfSquares := SumOfSquares(n)
+
+	return squareOfSum - sumOfSquares
 }
