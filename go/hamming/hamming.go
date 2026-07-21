43719dac3f9f9fe134e761f0d3cee1a82877db0a
TeePlunder <leon.bergmann23@gmail.com>
2026-07-21 08:42:47 +0200

    feat(go): solution hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,22 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return 0, errors.New("strings are not the same length")
+	}
+
+	differences := 0
+
+	for i := 0; i < len(a); i++ {
+		currentLetterA := a[i]
+		currentLetterB := b[i]
+
+		if currentLetterA != currentLetterB {
+			differences++
+		}
+	}
+
+	return differences, nil
 }
