cc88a3f89b4d76e6fd3b130b9d43a61469704c72
TeePlunder <leon.bergmann23@gmail.com>
2026-07-29 15:45:47 +0200

    feat(go): anagram current status

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,5 +1,28 @@
 package anagram
 
+import "strings"
+
 func Detect(subject string, candidates []string) []string {
+	lengthFilteredCanidates := filterOutLengthMatches(subject, candidates)
+
+	for i, element := range lengthFilteredCanidates {
+		lengthFilteredCanidates[i] = strings.ToLower(element)
+	}
+
 	panic("Please implement the Detect function")
+}
+
+func filterOutLengthMatches(subject string, candidates []string) []string {
+	subjectLength := len(subject)
+	var possibleCanidates []string
+
+	for _, element := range candidates {
+		if len(element) != subjectLength {
+			continue
+		}
+
+		possibleCanidates = append(possibleCanidates, element)
+	}
+
+	return possibleCanidates
 }
