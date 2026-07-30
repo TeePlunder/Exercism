d620f4b1bb802a7497e09c66d72f9f5ed60d79be
TeePlunder <leon.bergmann23@gmail.com>
2026-07-30 08:49:14 +0200

    feat(go): anagram check if letters are the same

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,6 +1,9 @@
 package anagram
 
-import "strings"
+import (
+	"slices"
+	"strings"
+)
 
 func Detect(subject string, candidates []string) []string {
 	lengthFilteredCanidates := filterOutLengthMatches(subject, candidates)
@@ -17,7 +20,41 @@ func Detect(subject string, candidates []string) []string {
 		uniqueCandidates = append(uniqueCandidates, element)
 	}
 
-	panic("Please implement the Detect function")
+	anagrams := []string{}
+	for _, element := range lengthFilteredCanidates {
+		if isSubjectAndCanidateTheSame(subject, element) {
+			anagrams = append(anagrams, element)
+		}
+	}
+
+	return anagrams
+}
+
+func isSubjectAndCanidateTheSame(subject, candidate string) bool {
+	var sortedSubject, sortedCandidate []string
+
+	for _, element := range subject {
+		sortedSubject = append(sortedSubject, string(element))
+	}
+
+	for _, element := range candidate {
+		sortedCandidate = append(sortedCandidate, string(element))
+	}
+
+	slices.Sort(sortedSubject)
+	slices.Sort(sortedCandidate)
+
+	// everything the same
+
+	isTheSame := true
+	for i := 0; i < len(sortedSubject); i++ {
+		if sortedSubject[i] != sortedCandidate[i] {
+			isTheSame = false
+			break
+		}
+	}
+
+	return isTheSame
 }
 
 func filterOutLengthMatches(subject string, candidates []string) []string {
