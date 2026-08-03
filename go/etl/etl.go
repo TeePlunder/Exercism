f6ab26cf0f43d7276bf57a99a2c5b32b05b17a4e
TeePlunder <leon.bergmann23@gmail.com>
2026-08-03 10:32:24 +0200

    feat(go): solution etl

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,5 +1,21 @@
 package etl
 
+import "strings"
+
+// old list
+// 1: []string{"A", "E"}, 2: []string{"D", "G"}
+
+// goal
+// "a": 1, "d": 2, "e": 1, "g": 2
+
 func Transform(in map[int][]string) map[string]int {
-	panic("Please implement the Transform function")
+	newList := make(map[string]int)
+	for letterScore, letters := range in {
+		for _, letter := range letters {
+			lowerCaseLetter := strings.ToLower(letter)
+			newList[lowerCaseLetter] = letterScore
+		}
+	}
+
+	return newList
 }
